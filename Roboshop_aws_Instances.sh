87bce68a83e64c7d57abe84b825523a69737fd8b
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-21 11:11:31 +0530

    AWS Instances

--- Roboshop_aws_Instances.sh
+++ Roboshop_aws_Instances.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-0c5e9ef09eaede486
+SG_ID=sg-0c5e9ef09eaede486 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-HOSTED_ZONE_ID=Z062815430DMB9RUXA69R
-DOMAIN_NAME=practiceazure.com
+ZONE_ID=Z062815430DMB9RUXA69R
+DOMAIN_NAME="practiceazure.com"
 
 for i in "${INSTANCES[@]}"
 do
-   
-   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-     then
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
         INSTANCE_TYPE="t3.small"
-     else
+    else
         INSTANCE_TYPE="t2.micro"
     fi
 
-IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0c5e9ef09eaede486 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}" --query 'Instances[0].PrivateIpAddress' --output text)
-    
-echo "$i: $IP_ADDRESS"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0c5e9ef09eaede486 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
 
-#create R53 record, make sure you delete existing record
+    #create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $HOSTED_ZONE_ID \
+    --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
         "Comment": "Creating a record set for cognito endpoint"
@@ -38,5 +36,5 @@ echo "$i: $IP_ADDRESS"
         }
         }]
     }
-
+        '
 done
