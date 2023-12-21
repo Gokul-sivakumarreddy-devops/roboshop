c79d082769972b3ef0710e15ac523ad8de19d8c3
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-21 08:09:57 +0530

    AWS Instances

---
+++ Roboshop_aws_Instances.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0c5e9ef09eaede486
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
+
+for i in "${INSTANCES[@]}"
+do
+   echo "Instance is: $i"
+   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+     then
+        INSTANCE_TYPE="t3.small"
+     else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+done
+
+aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-0c5e9ef09eaede486
